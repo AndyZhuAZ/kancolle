c42b2cf951b054ff85b27c21a61859e5d88b738d
andy <andy.chu.cs@icloud.com>
2023-12-19 00:34:18 +0800

    feat: kancolle data: ship, ship_type, ship_class

---
+++ kancolle/data/__init__.py
@@ -0,0 +1,6 @@
+"""Data module."""
+import importlib.resources
+
+MODULE_PATH = importlib.resources.files(__package__)
+KC_DATA_URL = "https://andychucs.github.io/kcwiki-kcdata/"
+KC_QUEST_URL = "https://andychucs.github.io/kcQuest/"
--- tests/test_kancolle.py
+++ tests/test_kancolle.py
@@ -8,20 +8,57 @@ from kancolle import cli
 
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
+def ship_data_response():
+    """Ship data response."""
+    from kancolle.data import KC_DATA_URL
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    ship_data_url = KC_DATA_URL + "ship/ship.json"
+    import requests  # type: ignore
+
+    return requests.get(ship_data_url).json()
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-    del response
+@pytest.fixture
+def ship_type_response():
+    """Ship type response."""
+    from kancolle.data import KC_DATA_URL
+
+    ship_type_url = KC_DATA_URL + "shiptype/all.json"
+    import requests  # type: ignore
+
+    return requests.get(ship_type_url).json()
+
+
+@pytest.fixture
+def ship_class_response():
+    """Ship class response."""
+    from kancolle.data import KC_DATA_URL
+
+    ship_class_url = KC_DATA_URL + "shipclass/all.json"
+    import requests  # type: ignore
+
+    return requests.get(ship_class_url).json()
+
+
+def test_load_ship_data(ship_data_response, ship_class_response, ship_type_response):
+    """Test load ship data."""
+    from kancolle.models import ship
+    from kancolle.models import ship_class
+    from kancolle.models import ship_type
+
+    ships = ship.load_ships(ship_data_response)
+    ship_classes = ship_class.load_ship_class_list(ship_class_response)
+    ship_types = ship_type.load_ship_type_list(ship_type_response)
+
+    assert ships[0].name == '睦月'
+
+    class_name = next((sc.name for sc in ship_classes if sc.id == ships[0].class_id), '')
+
+    assert ships[0].name in class_name
+
+    type_name = next((st.name for st in ship_types if st.id == ships[0].type_id), '')
+
+    assert type_name == "駆逐艦"
 
 
 def test_command_line_interface():
