ea289203deee4c601695c23881803b2155bbc046
andy <andy.chu.cs@icloud.com>
2023-12-19 00:44:48 +0800

    fix: AttributeError on python 3.8

--- kancolle/data/__init__.py
+++ kancolle/data/__init__.py
@@ -1,6 +1,10 @@
 """Data module."""
 import importlib.resources
+import os.path
 
-MODULE_PATH = importlib.resources.files(__package__)
+try:
+    MODULE_PATH = importlib.resources.files(__package__)
+except AttributeError:
+    MODULE_PATH = os.path.abspath(os.path.dirname(__file__))
 KC_DATA_URL = "https://andychucs.github.io/kcwiki-kcdata/"
 KC_QUEST_URL = "https://andychucs.github.io/kcQuest/"
