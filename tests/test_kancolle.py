7ee462c19eae44e3bbed7b39ff5adce01870b512
andy <andy.chu.cs@icloud.com>
2024-07-27 14:32:45 +0800

    feat: add slotitem

--- tests/test_kancolle.py
+++ tests/test_kancolle.py
@@ -46,7 +46,7 @@ def test_load_ship_data(ship_data_response, ship_class_response, ship_type_respo
     from kancolle.models import ship_class
     from kancolle.models import ship_type
 
-    ships = ship.load_ships(ship_data_response)
+    ships = ship.load_ship_list(ship_data_response)
     ship_classes = ship_class.load_ship_class_list(ship_class_response)
     ship_types = ship_type.load_ship_type_list(ship_type_response)
 
@@ -81,3 +81,43 @@ def test_data_download():
     assert type(data.load_ship_data()) == list
     assert type(data.load_ship_class_data()) == list
     assert type(data.load_ship_type_data()) == list
+
+
+def test_data_load():
+    """Test data load."""
+    from kancolle.data import data
+    from kancolle.models import ship, ship_type, ship_class, slot_item
+
+    assert type(data.load_ship_data()) == list
+    assert type(data.load_ship_class_data()) == list
+    assert type(data.load_ship_type_data()) == list
+
+    ships = ship.load_ship_list(data.load_ship_data())
+    ship_classes = ship_class.load_ship_class_list(data.load_ship_class_data())
+    ship_types = ship_type.load_ship_type_list(data.load_ship_type_data())
+
+    assert ships[0].name == '睦月'
+
+    class_name = next((sc.name for sc in ship_classes if sc.id == ships[0].class_id), '')
+
+    assert ships[0].name in class_name
+
+    type_name = next((st.name for st in ship_types if st.id == ships[0].type_id), '')
+
+    assert type_name == "駆逐艦"
+
+    assert type(data.load_slot_item_data()) == list
+
+    slot_items = slot_item.load_slot_item_list(data.load_slot_item_data())
+
+    assert slot_items[0].name == '12cm単装砲'
+
+
+def test_kancolle_class():
+    """Test Kancolle class."""
+    import kancolle.kancolle as kc
+
+    kancolle_data = kc.Kancolle()
+
+    assert kancolle_data.ships[0].name == "睦月"
+    assert kancolle_data.slot_items[0].name == "12cm単装砲"
